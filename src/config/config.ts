7191cbcdd40dad503cc39b949de5fce37565f6a8
Radu-Vlad NICULAE <rniculae@192-168-0-218.rdsnet.ro>
2022-06-04 15:18:26 +0300

    update gitignore

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 const path = require("path");
 
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 const MYSQL = {
   host: process.env.HOST,
